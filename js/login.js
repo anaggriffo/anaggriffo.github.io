5d7d56311dc488b6fd2f7e1fc3824364a679070e
anaggriffo <54519028+anaggriffo@users.noreply.github.com>
2019-10-06 22:16:02 -0300

    Add files via upload

--- js/login.js
+++ js/login.js
@@ -1,14 +1,14 @@
-function hacerLogin(){
+function hacerLogin() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
 
     localStorage.setItem("username", username);
-    
-    if (username =="anaggriffo" && password == "!Clave25"){
+
+    if (username == "anaggriffo" && password == "!Clave25") {
         window.location = "index.html"
         return false;
     }
-    else{
+    else {
         alert("Usuario o contraseña incorrectos, verifique los datos e intente nuevamente");
         window.location = "index.html"
         return false;
@@ -22,13 +22,14 @@ function onSignIn(googleUser) {
     console.log('Image URL: ' + profile.getImageUrl());
     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
     var id_token = googleUser.getAuthResponse().id_token;
-    console.log(id_token);  
+    console.log(id_token);
     window.location = "index.html"
 }
 function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
-      console.log('User signed out.');
+        console.log('User signed out.');
+        window.location = "login.html"
+
     });
-    window.location = "login.html"
-  }
+}
