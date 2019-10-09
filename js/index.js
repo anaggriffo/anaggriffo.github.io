3519487dd7a77fb3b6a904be6f25b85f981a1a38
anaggriffo <54519028+anaggriffo@users.noreply.github.com>
2019-10-09 19:32:39 -0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -6,21 +6,17 @@ function showUsername() {
 }
 showUsername()
 
-function signOut() {
-
-    var signout = window.location.replace("login.html");
-    document.getElementById("signout").innerHTML = signout;
-    
-}
-function verCarrito(){
+function verCarrito() {
 
     window.location.assign("cart.html");
     document.getElementById("vercarrito").innerHTML;
 }
 function signOut() {
+    var signout = window.location.replace("login.html");
+    document.getElementById("signout").innerHTML = signout;
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
-      console.log('User signed out.');
-      window.location = "login.html"
+        console.log('User signed out.');
+        window.location = "login.html"
     });
-  }
+}
