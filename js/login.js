79c33d6869aa9d94cc41f0f14c551a40ece29328
anaggriffo <54519028+anaggriffo@users.noreply.github.com>
2019-10-09 20:09:39 -0300

    Add files via upload

--- js/login.js
+++ js/login.js
@@ -1,14 +1,14 @@
-function hacerLogin(){
+function hacerLogin() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
 
     localStorage.setItem("username", username);
-    
-    if (username =="anaggriffo" && password == "!Clave25"){
+
+    if (username == "anaggriffo" && password == "!Clave25") {
         window.location = "index.html"
         return false;
     }
-    else{
+    else {
         alert("Usuario o contraseña incorrectos, verifique los datos e intente nuevamente");
         window.location = "index.html"
         return false;
@@ -17,19 +17,20 @@ function hacerLogin(){
 
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
+    localStorage.setItem('Name: ', username);
     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
     console.log('Name: ' + profile.getName());
     console.log('Image URL: ' + profile.getImageUrl());
     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
     var id_token = googleUser.getAuthResponse().id_token;
     console.log(id_token);
-    window.location = "index.html"  
+    window.location = "index.html"
 }
 
- function signOut() {
+function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
-      console.log('User signed out.');
-      window.location = "login.html"
+        console.log('User signed out.');
+        window.location = "login.html"
     });
-  }
+}
